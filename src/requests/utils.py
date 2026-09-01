ae9ade1d5754411f179553ab341a85b063284386
ap555-sys <298232079+ap555-sys@users.noreply.github.com>
2026-09-01 13:46:01 +0530

    Update utils.py

--- src/requests/utils.py
+++ src/requests/utils.py
@@ -577,7 +577,7 @@ def get_encoding_from_headers(headers: CaseInsensitiveDict[str]) -> str | None:
     content_type = headers.get("content-type")
 
     if not content_type:
-        return None
+        return "utf-8"
 
     content_type, params = _parse_content_type_header(content_type)
 
